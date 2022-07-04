51d41973f23932f3adf91c405635d2dcdad0b30e
KDH_Notebook <destiny1017@nate.com>
2022-07-04 17:29:24 +0900

    Initial commit

---
+++ src/main/java/com/typetest/TypetestApplication.java
@@ -0,0 +1,13 @@
+package com.typetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/typetest/TypetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
