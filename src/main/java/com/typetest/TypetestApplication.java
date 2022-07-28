0ac989c76275e36c7cbf3cda0b16292a9380313e
KDH_Notebook <destiny1017@nate.com>
2022-07-28 20:30:44 +0900

    default timezone 설정(Asia/Seoul)

--- src/main/java/com/typetest/TypetestApplication.java
+++ src/main/java/com/typetest/TypetestApplication.java
@@ -4,8 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import javax.annotation.PostConstruct;
+import java.sql.Time;
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TypetestApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TypetestApplication.class, args);
