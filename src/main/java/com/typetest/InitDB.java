9021fba0c10b3968ed47030882e2866aa4708fcb
KDH_Notebook <destiny1017@nate.com>
2022-08-06 09:59:05 +0900

    initDB 테스트 환경일 때는 실행 안 되게 변경

--- src/main/java/com/typetest/InitDB.java
+++ src/main/java/com/typetest/InitDB.java
@@ -8,6 +8,7 @@ import com.typetest.personalities.domain.TypeInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,32 +32,35 @@ public class InitDB {
     static class InitService {
 
         private final EntityManager em;
+        private final Environment env;
 
         public void init() {
-            TypeInfo typeInfo1 = new TypeInfo(TestCode.EXAM, "BBA", "비비에이");
-            TypeInfo typeInfo2 = new TypeInfo(TestCode.EXAM, "AAA", "에에에이");
-            TypeInfo typeInfo3 = new TypeInfo(TestCode.EXAM, "BAB", "비에이비");
-            TypeInfo typeInfo4 = new TypeInfo(TestCode.MBTI, "INTP", "인팁");
-            TypeInfo typeInfo5 = new TypeInfo(TestCode.EXAM, "BBB", "비비비이");
-            em.persist(typeInfo1);
-            em.persist(typeInfo2);
-            em.persist(typeInfo3);
-            em.persist(typeInfo4);
-            em.persist(typeInfo5);
+            if(!env.getProperty("spring.profiles.active").equals("test")) {
+                TypeInfo typeInfo1 = new TypeInfo(TestCode.EXAM, "BBA", "비비에이");
+                TypeInfo typeInfo2 = new TypeInfo(TestCode.EXAM, "AAA", "에에에이");
+                TypeInfo typeInfo3 = new TypeInfo(TestCode.EXAM, "BAB", "비에이비");
+                TypeInfo typeInfo4 = new TypeInfo(TestCode.MBTI, "INTP", "인팁");
+                TypeInfo typeInfo5 = new TypeInfo(TestCode.EXAM, "BBB", "비비비이");
+                em.persist(typeInfo1);
+                em.persist(typeInfo2);
+                em.persist(typeInfo3);
+                em.persist(typeInfo4);
+                em.persist(typeInfo5);
 
-            User user = new User("김대호",
-                    "eogh6428@gmail.com",
-                    "https://lh3.googleusercontent.com/a-/AFdZucr_8gjDmt791JrOHftPA1UX3kvt1WiRxW19AH4JdQ=s96-c",
-                    Role.USER, "디앙");
-            PersonalityType pt1 = new PersonalityType(user, TestCode.EXAM, "AAA");
-            PersonalityType pt2 = new PersonalityType(user, TestCode.EXAM, "BBA");
-            PersonalityType pt3 = new PersonalityType(user, TestCode.MBTI, "INTP");
-            em.persist(user);
-            em.persist(pt1);
-            em.persist(pt2);
-            em.persist(pt3);
+                User user = new User("김대호",
+                        "eogh6428@gmail.com",
+                        "https://lh3.googleusercontent.com/a-/AFdZucr_8gjDmt791JrOHftPA1UX3kvt1WiRxW19AH4JdQ=s96-c",
+                        Role.USER, "디앙");
+                PersonalityType pt1 = new PersonalityType(user, TestCode.EXAM, "AAA");
+                PersonalityType pt2 = new PersonalityType(user, TestCode.EXAM, "BBA");
+                PersonalityType pt3 = new PersonalityType(user, TestCode.MBTI, "INTP");
+                em.persist(user);
+                em.persist(pt1);
+                em.persist(pt2);
+                em.persist(pt3);
 
-            em.flush();
+                em.flush();
+            }
 
         }
     }
