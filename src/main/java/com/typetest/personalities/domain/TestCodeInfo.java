b6fc3b48b9a053f36225d0792d697876099fa321
KDH_Notebook <destiny1017@nate.com>
2022-08-08 23:20:28 +0900

    TestCodeInfo 엔티티 추가

---
+++ src/main/java/com/typetest/personalities/domain/TestCodeInfo.java
@@ -0,0 +1,31 @@
+package com.typetest.personalities.domain;
+
+import com.typetest.personalities.data.AnswerType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+@RequiredArgsConstructor
+public class TestCodeInfo {
+
+    @Id
+    private String testCode;
+
+    private String testName;
+
+    @Enumerated(EnumType.STRING)
+    private AnswerType answerType;
+
+    public TestCodeInfo(String testCode, String testName, AnswerType answerType) {
+        this.testCode = testCode;
+        this.testName = testName;
+        this.answerType = answerType;
+    }
+}
