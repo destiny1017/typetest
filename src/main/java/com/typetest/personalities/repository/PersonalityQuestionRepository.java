0e59ab4324ef599d7770c97d7c8fecec82302741
KDH_Notebook <destiny1017@nate.com>
2022-08-14 21:27:30 +0900

    findByTestCode pageable쿼리 jpql형식으로 변경

--- src/main/java/com/typetest/personalities/repository/PersonalityQuestionRepository.java
+++ src/main/java/com/typetest/personalities/repository/PersonalityQuestionRepository.java
@@ -2,14 +2,21 @@ package com.typetest.personalities.repository;
 
 import com.typetest.personalities.domain.PersonalityQuestion;
 import com.typetest.personalities.domain.TestCodeInfo;
+import com.typetest.personalities.exam.dto.ExamQuestionDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PersonalityQuestionRepository extends JpaRepository<PersonalityQuestion, Long> {
     List<PersonalityQuestion> findByTestCode(TestCodeInfo testCode);
-    Page<PersonalityQuestion> findByTestCode(TestCodeInfo testCodeInfo, Pageable pageable);
-    Long countByTestCode(String testCode);
+
+    @Query("select new com.typetest.personalities.exam.dto.ExamQuestionDto(pq) " +
+            "from PersonalityQuestion pq " +
+            "where pq.testCode = :testCode")
+    Page<ExamQuestionDto> findByTestCode(@Param("testCode") TestCodeInfo testCodeInfo, Pageable pageable);
+    Long countByTestCode(TestCodeInfo testCode);
 }
