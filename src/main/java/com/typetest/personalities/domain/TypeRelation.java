f55c3578762f433cc80170d41f2af4f8ab22e4c6
KDH_Notebook <destiny1017@nate.com>
2022-08-18 23:10:53 +0900

    TypeRelation 엔티티 추가

---
+++ src/main/java/com/typetest/personalities/domain/TypeRelation.java
@@ -0,0 +1,39 @@
+package com.typetest.personalities.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class TypeRelation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "TYPE_INFO_ID")
+    private TypeInfo typeInfo;
+
+    @ManyToOne
+    @JoinColumn(name = "BEST_TYPE")
+    private TypeInfo bestType;
+
+    @ManyToOne
+    @JoinColumn(name = "WORST_TYPE")
+    private TypeInfo worstType;
+
+    public TypeRelation(TypeInfo typeInfo, TypeInfo bestType) {
+        this.typeInfo = typeInfo;
+        this.bestType = bestType;
+    }
+
+    public TypeRelation(TypeInfo typeInfo, TypeInfo bestType, TypeInfo worstType) {
+        this.typeInfo = typeInfo;
+        this.bestType = bestType;
+        this.worstType = worstType;
+    }
+}
