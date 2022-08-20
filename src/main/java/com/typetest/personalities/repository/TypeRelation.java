b9189669c28ebfb589f055c196e9e81ff3f809aa
KDH_Notebook <destiny1017@nate.com>
2022-08-20 11:57:21 +0900

    TypeRelationRepository 클래스명 잘못 설정하여 변경

--- src/main/java/com/typetest/personalities/repository/TypeRelation.java
+++
@@ -1,6 +0,0 @@
-package com.typetest.personalities.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeRelation extends JpaRepository<TypeRelation, Long> {
-}
---
+++ src/main/java/com/typetest/personalities/repository/TypeRelationRepository.java
@@ -0,0 +1,7 @@
+package com.typetest.personalities.repository;
+
+import com.typetest.personalities.domain.TypeRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRelationRepository extends JpaRepository<TypeRelation, Long> {
+}
