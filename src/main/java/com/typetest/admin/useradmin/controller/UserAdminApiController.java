43919ffb2000f9003d0ecb4383baf6793709f94f
KDH_Notebook <destiny1017@nate.com>
2023-01-07 18:14:42 +0900

    RestAPI ResponseEntity 적용

--- src/main/java/com/typetest/admin/useradmin/controller/UserAdminApiController.java
+++ src/main/java/com/typetest/admin/useradmin/controller/UserAdminApiController.java
@@ -5,6 +5,8 @@ import com.typetest.admin.useradmin.service.UserAdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,23 +18,27 @@ public class UserAdminApiController {
     private final UserAdminService userAdminService;
 
     @GetMapping("/api/user-admin/users")
-    public Page<UserInfoDto> getUserList(Pageable pageable) {
-        return userAdminService.getUserList(pageable);
+    public ResponseEntity getUserList(Pageable pageable) {
+        Page<UserInfoDto> userList = userAdminService.getUserList(pageable);
+        return new ResponseEntity(userList, HttpStatus.OK);
     }
 
     @GetMapping("/api/user-admin/users/{name}")
-    public List<UserInfoDto> findUserInfo(@PathVariable String name) {
-        return userAdminService.findUserInfo(name);
+    public ResponseEntity findUserInfo(@PathVariable String name) {
+        List<UserInfoDto> userInfo = userAdminService.findUserInfo(name);
+        return new ResponseEntity(userInfo, HttpStatus.OK);
     }
 
     @PutMapping("/api/user-admin/users")
-    public UserInfoDto updateUserInfo(@RequestBody UserInfoDto userInfoDto) {
-        return userAdminService.updateUserInfo(userInfoDto);
+    public ResponseEntity updateUserInfo(@RequestBody UserInfoDto userInfoDto) {
+        UserInfoDto updatedUserInfoDto = userAdminService.updateUserInfo(userInfoDto);
+        return new ResponseEntity(updatedUserInfoDto, HttpStatus.OK);
     }
 
     @DeleteMapping("/api/user-admin/users/{id}")
-    public void deleteUserInfo(@PathVariable Long id) {
+    public ResponseEntity deleteUserInfo(@PathVariable Long id) {
         userAdminService.deleteUserInfo(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
