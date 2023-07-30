927c5c0ab83c73ab03b0abe0920c6951729257a6
KDH_Notebook <destiny1017@nate.com>
2023-07-30 14:44:15 +0900

    tomcat header 설정 수정

---
+++ src/main/java/com/typetest/config/CacheControlInterceptor.java
@@ -0,0 +1,16 @@
+package com.typetest.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CacheControlInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setHeader("Cache-Control", "public");
+        response.setHeader("Pragma", "public");
+        return true;
+    }
+}
