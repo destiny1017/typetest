b8eac9a71233dfe07404c844210e29132dcb66f5
eogh6428@naver.com <destiny1017@nate.com>
2024-01-27 17:06:12 +0900

    Fix: 테스트시 SecurityFilter 제외 코드 수정
    filter가 아니라 auto configure로 빌드되는 방식이라 제거가 안 되었음. @AutoConfigureMockMvc를 통해 제외하도록 변경.

--- src/test/java/com/typetest/ControllerTestSupport.java
+++ src/test/java/com/typetest/ControllerTestSupport.java
@@ -3,19 +3,14 @@ package com.typetest;
 import com.typetest.admin.testadmin.controller.TestAdminController;
 import com.typetest.config.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
         TestAdminController.class
-}, excludeFilters = {
-        @ComponentScan.Filter(
-                type = FilterType.ASSIGNABLE_TYPE,
-                classes = SecurityConfig.class
-        )
-})
+}, excludeAutoConfiguration = SecurityConfig.class)
+@AutoConfigureMockMvc(addFilters = false)
 public abstract class ControllerTestSupport {
 
     @Autowired
